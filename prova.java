02e7c7b7c6067621f100fede28e9c214f3d805cd
AlfO8 <alfonso@ubuntu.(none)>
2012-02-07 09:21:06 -0800

    Prova2

--- prova.java
+++ prova.java
@@ -1,3 +1,6 @@
-public static void main(String [] args){
- 	System.out.println("Prova GitHub");
+public class Prova{
+	public static void main(String [] args){
+	for(int i=0; i<2; i++;);
+ 		System.out.println("Prova GitHub" + i);
+	}
 }
